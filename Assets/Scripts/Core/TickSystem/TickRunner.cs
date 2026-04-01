37345e5b8216179a5c6772aa5c1acac39fe207e2
Ferron <37308@ma-web.nl>
2026-04-01 16:16:54 +0200

    tick system + battlebar example

---
+++ Assets/Scripts/Core/TickSystem/TickRunner.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TickRunner : MonoBehaviour
+{
+    public UnityEvent<float> OnAttack;
+    
+    [SerializeField] private TickPattern tickPattern;
+    [SerializeField] private float speedMultiplier = 1;
+    [SerializeField] private float damageMultiplier = 1;
+    
+
+    public void Run()
+    {
+        if (_runTickCoroutine != null) StopCoroutine(_runTickCoroutine);
+        _runTickCoroutine = StartCoroutine(RunTickPattern());
+    }
+
+    public void Stop()
+    {
+        if (_runTickCoroutine != null) StopCoroutine(_runTickCoroutine);
+    }
+
+    private IEnumerator RunTickPattern()
+    {
+        foreach (var tick in tickPattern.Ticks)
+        {
+            OnAttack.Invoke(tick.Damage * damageMultiplier);
+            yield return new WaitForSeconds(tick.Duration * speedMultiplier);
+        }
+
+        _runTickCoroutine = StartCoroutine(RunTickPattern());
+    }
+}
