960ef38cdd484b79a27576f87fc7f1a063cc4a3d
Tenseven-107 <nadrichem@tanous.nl>
2026-04-08 14:38:36 +0200

    Added a script to save and load robots

---
+++ Assets/Scripts/Core/PartSystem/RobotBuilder.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yakapedia;
+
+public class RobotBuilder : MonoBehaviour
+{
+    [SerializeField] private Part DefaultHead;
+    [SerializeField] private Part DefaultWeapon;
+    [SerializeField] private Part DefaultBody;
+    [SerializeField] private Part DefaultCpu;
+    [SerializeField] private Part DefaultMovement;
+
+    private const string HeadKey = "Head"; 
+    private const string WeaponKey = "Weapon"; 
+    private const string BodyKey = "Body"; 
+    private const string CpuKey = "Cpu"; 
+    private const string MovementKey = "Movement";
+    
+
+    public Robot LoadRobot()
+    {
+        var robot = new Robot();
+
+        var head = PersistentData.Get(HeadKey, DefaultHead);
+        var weapon = PersistentData.Get(WeaponKey, DefaultWeapon);
+        var body = PersistentData.Get(BodyKey, DefaultBody);
+        var cpu = PersistentData.Get(CpuKey, DefaultCpu);
+        var movement = PersistentData.Get(MovementKey, DefaultMovement);
+
+        robot.Head = head;
+        robot.Weapon = weapon;
+        robot.Body = body;
+        robot.Cpu = cpu;
+        robot.MovementModule = movement;
+
+        return robot;
+    }
+
+    public void ChangeRobotPart(Part newPart)
+    {
+        switch (newPart.Type)
+        {
+            case Part.PartType.Head: PersistentData.Set(HeadKey, newPart); break;
+            case Part.PartType.Weapon: PersistentData.Set(WeaponKey, newPart); break;
+            case Part.PartType.Body: PersistentData.Set(BodyKey, newPart); break;
+            case Part.PartType.Cpu: PersistentData.Set(CpuKey, newPart); break;
+            case Part.PartType.MovementModule: PersistentData.Set(MovementKey, newPart); break;
+        }
+    }
+}
