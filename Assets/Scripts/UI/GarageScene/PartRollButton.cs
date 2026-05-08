0c939e5572812263c9a8e413550d4960befb71fc
Tenseven-107 <nadrichem@tanous.nl>
2026-05-08 13:59:48 +0200

    Added scaling to parts

--- Assets/Scripts/UI/GarageScene/PartRollButton.cs
+++ Assets/Scripts/UI/GarageScene/PartRollButton.cs
@@ -10,7 +10,6 @@ public class PartRollButton : MonoBehaviour
 {
     [SerializeField] private int cost = 10;
     [SerializeField] private PartCollection partCollection;
-    // ADD SCALING OBJECT HERE
     [SerializeField] private PartsScreen partScreen;
 
     public UnityEvent OnEnough;
@@ -27,9 +26,10 @@ public class PartRollButton : MonoBehaviour
                 return;
             }
             MoneyBag.Remove(cost);
-
             OnEnough.Invoke();
-            partScreen.ComparePart(partCollection.GetRandomPart());
+
+            var part = PartUtility.ScalePart(partCollection.GetRandomPart(), RankSystem.CurrentRank);
+            partScreen.ComparePart(part);
         });
 
         var buttonText = GetComponentInChildren<TextMeshProUGUI>();
